abd1b1484044f4c146b1c2af9ecc5b17fdd7408b
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-07-30 22:59:55 -0600

    add dynamic path to show a user based on id
    in app.module, set users/:id to render the user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,9 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
-  {path: "users", component: UsersComponent },
   {path: "", component: HomeComponent },
+  {path: "users", component: UsersComponent },
+  {path: "users/:id", component: UserComponent },
   {path: "servers", component: ServersComponent }
 ];
 
