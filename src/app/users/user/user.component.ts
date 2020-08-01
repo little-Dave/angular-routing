972ba9a94906148b9e62c82bb122f5b1f2d22e79
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-07-31 23:07:15 -0600

    access params and use info to dynamically inform template
    utitlize @angular/router's ActivadRoute module to access params and pass them to the user object rendered by the template

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { identity } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const { id, name } = this.route.snapshot.params;
+    this.user = {
+      id: id,
+      name: name
+    }
+    // console.log(this.route.snapshot.params)
   }
 
 }
