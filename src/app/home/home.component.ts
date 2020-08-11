3ab53493a9da9d59b6594702fabc594e0763baca
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-08-10 22:29:18 -0600

    break onLoadServers() into multiple lines

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,9 @@ export class HomeComponent implements OnInit {
   }
 
   onLoadServers(id: number) {
-    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: 1}, fragment: 'loading'});
+    this.router.navigate(['/servers', id, 'edit'], 
+                         {queryParams: {allowEdit: 1}, 
+                         fragment: 'loading'});
   }
 
 }
