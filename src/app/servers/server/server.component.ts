d368b72d3798007d63854d7951ae04ec5a08d821
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-08-11 21:58:37 -0600

    create onEdit() in server.component
    onEdit will navigate to /servers/:id/edit by appending edit to current path, which already includes /servers and a unique id

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,7 +12,8 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
@@ -22,6 +23,10 @@ export class ServerComponent implements OnInit {
           this.server = this.serversService.getServer(+params['id']);
         }
       );
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
