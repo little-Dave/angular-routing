2c3bab2ebe56ffef6f2006d8f3939df77f95654e
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-08-21 23:09:09 -0600

    import and inject AuthService into auth-guard.service
    call AuthService's isAuthenticated method and grant or deny user request based on the promise's resolution

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,10 +1,26 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 
 import { Observable } from "rxjs/observable";
+import { Injectable } from "@angular/core";
 
+import { AuthService } from "./auth.service";
+
+@Injectable
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService,
+              private router: Router) {}
+
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    
+    this.authService.isAuthenticated();
+      .then(
+        (authenticated: boolean) => {
+          if (authenticated) {
+            return true;
+          } else {
+            this.router.navigate([""]);
+          }
+        }
+      )
   }
 }
