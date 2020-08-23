67fdeac6fcb90c5b4fd67b5dae38e06018d51e3b
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-08-22 23:09:22 -0600

    protect child routes of /servers
    import and implement CanActivateChild in auth-guard.service;
    update app-routing module's /servers path to use canActivateChild rather than canActivate

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,4 +1,10 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { 
+  CanActivate, 
+  ActivatedRouteSnapshot, 
+  RouterStateSnapshot, 
+  Router, 
+  CanActivateChild 
+} from "@angular/router";
 
 import { Observable } from "rxjs/observable";
 import { Injectable } from "@angular/core";
@@ -6,7 +12,7 @@ import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanActivateChild{
   constructor(private authService: AuthService,
               private router: Router) {}
 
@@ -22,5 +28,9 @@ export class AuthGuardService implements CanActivate {
           }
         }
       );
+  }
+  canActivateChild(route: ActivatedRouteSnapshot,
+                   state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
   }
 }
