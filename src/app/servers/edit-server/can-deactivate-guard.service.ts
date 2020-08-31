9f9e64f06481d64b4a4e45b8eb30701a84a15dcc
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-08-30 22:06:11 -0600

    add logic to CanDeactivateGuard export
    pass typed arguments and define return value

--- src/app/servers/edit-server/can-deactivate-guard.service.ts
+++ src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -1,4 +1,4 @@
-import { CanDeactivate } from '@angular/router';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { Observable } from "rxjs";
 
@@ -7,5 +7,10 @@ export interface CanComponentDeactivate {
 }
 
 export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
-
+  canDeactivate(component: CanComponentDeactivate,
+                currentRoute: ActivatedRouteSnapshot,
+                currentState: RouterStateSnapshot,
+                nextState?: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return component.canDeactivate();
+    }
 }
