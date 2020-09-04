476c632f0bf3c4e38ffac610e022a10ed32101bb
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-09-03 23:42:12 -0600

    mkdir error-page; touch error-page.ts, .html, .css
    write boilerplate for .ts file;
    import and register component in app.module

---
+++ src/app/error-page/error-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'error-page',
+  templateUrl: './error-page.component.html',
+  styleUrls: ['./error-page.component.css']
+})
+
+export class ErrorPageComponent {
+  
+}
